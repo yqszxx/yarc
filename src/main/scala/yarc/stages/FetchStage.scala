b76f54761b981300b1be6b8472984ad30a4f9a7f
yqszxx <yqszxx@gmail.com>
2019-08-19 21:05:17 +0800

    Clean the mess of FetchStage.

--- src/main/scala/yarc/stages/FetchStage.scala
+++ src/main/scala/yarc/stages/FetchStage.scala
@@ -8,26 +8,17 @@ import chisel3.experimental.ChiselEnum
 
 object FetchStage {
   object ProgramCounterSource extends ChiselEnum  {
-    val pcPlus4,
-        branchTarget,
-        jalrTarget,
-        exceptionVector = Value
+    val pcPlus4 = Value
   }
 }
 
-object FetchStageIO extends Bundle {
-  import FetchStage._
+class FetchStageControlSignals extends Bundle {
+}
 
-  val control = new Bundle {
-//    val pcSource = Input(ProgramCounterSource())
-//    val stall    = Input(Bool())
-//    val kill     = Input(Bool())
-  }
+class FetchStageIO extends Bundle {
+  val control = new FetchStageControlSignals
 
   val data = new Bundle {
-//    val branchTarget             = Input(UInt(32.W))
-//    val jalrTarget               = Input(UInt(32.W))
-//    val exceptionVector          = Input(UInt(32.W))
     val pc                       = Output(UInt(32.W))
     val instruction              = Output(UInt(32.W))
   }
@@ -37,36 +28,18 @@ object FetchStageIO extends Bundle {
 }
 
 class FetchStage extends Module {
-  import FetchStage._
-  val io = IO(FetchStageIO)
+  val io = IO(new FetchStageIO)
 
   val programCounter = RegInit("h0000".U(32.W))
   val pcPlus4 = programCounter + 4.U
 
-  // program counter source mux
-//  val pcSource = MuxLookup(
-//    io.control.pcSource.asUInt,
-//    pcPlus4,
-//    Array(
-//      ProgramCounterSource.pcPlus4.asUInt -> pcPlus4,
-//      ProgramCounterSource.branchTarget.asUInt -> io.data.branchTarget,
-//      ProgramCounterSource.jalrTarget.asUInt -> io.data.jalrTarget,
-//      ProgramCounterSource.exceptionVector.asUInt -> io.data.exceptionVector
-//    )
-//  )
   val pcSource = pcPlus4
 
-  //  when (!io.control.stall) {
   programCounter := pcSource
-  //  }
 
   io.data.pc := programCounter
   io.mem.address := programCounter
 
-//  val bubble = "h00000013".U(32.W) // add x0, x0, x0
-  // instruction mux
-//  val instruction = Mux(io.control.kill, bubble, io.mem.readData)
-//  io.data.instruction := instruction
   io.data.instruction := io.mem.readData
   printf(p"Fetching Instruction: 0x${Hexadecimal(io.mem.address)} -> 0x${Hexadecimal(io.data.instruction)}\n")
 }
